67bd11ff4ee47ae96012193ff0016550a8bbe022
Raphael <puello322@gmail.com>
2023-05-07 19:46:58 -0600

    Adding s3 code

--- camera_test.py
+++ camera_test.py
@@ -1,12 +1,20 @@
+import boto3
+from dotenv import load_dotenv
+import os
 import time
 from picamera2 import Picamera2
 
-# Initialize the camera
+
+# Load .env file
+load_dotenv()
+
+my_bucket = os.environ.get('S3_BUCKET')
+s3 = boto3.resource('s3')
+
+# Upload a new file
+data = open('test.jpg', 'rb')
+s3.Bucket(my_bucket).put_object(Key='test.jpg', Body=data)
+
+# Initialize camera
 camera = Picamera2()
-
-# Capture the image
-camera.start_and_capture_file('test_image.jpg')
-
-# Close the camera
-camera.close()
-
+interval = 500
