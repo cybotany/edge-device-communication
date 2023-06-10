07fa86f249df2c59f6d7a8cd504f0c4dfbbbddb0
Raphael <puello322@gmail.com>
2023-06-09 21:51:11 -0600

    Updating API

--- main.py
+++ main.py
@@ -29,7 +29,25 @@ def get_unique_id():
 def post_to_web_server(ip_address, unique_id):
     # Post to web server
     url = config('URL')
-    response = requests.post(f'{url}/identify_cea/', data={
+
+    # Get JWT
+    token_response = requests.post(f'{url}/api/token/', data={
+        'username': config('USERNAME'),
+        'password': config('PASSWORD'),
+    })
+    
+    if token_response.status_code != 200:
+        print("Failed to obtain JWT.")
+        print(token_response.text)
+        return
+
+    token = token_response.json().get('access')
+
+    headers = {
+        'Authorization': f'Bearer {token}',
+    }
+
+    response = requests.post(f'{url}/identify_cea/', headers=headers, data={
         'ip_address': ip_address,
         'identifier': unique_id,
     })
@@ -39,6 +57,7 @@ def post_to_web_server(ip_address, unique_id):
     else:
         print("Failed to register Raspberry Pi.")
         print(response.text)
+
 
 
 def capture_image(filename):
