14eb58ca416bafc61353190331b22ceb6bfdc38d
Raphael <puello322@gmail.com>
2023-08-11 19:45:13 -0600

    correction

--- light_control.py
+++ light_control.py
@@ -9,7 +9,7 @@ async def main():
     print(p.alias)  # Print out the alias
     print(p.emeter_realtime)  # Print out current emeter status
 
-    await p.turn_off()  # Turn the device off
+    await p.turn_on()  # Turn the device off
 
 if __name__ == "__main__":
     asyncio.run(main())
