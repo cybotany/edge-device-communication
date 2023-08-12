14853c023bfbd81e3c26b69fd75c6463f58abcab
Raphael <puello322@gmail.com>
2023-08-11 19:44:57 -0600

    More fixes to light control

--- light_control.py
+++ light_control.py
@@ -1,6 +1,15 @@
 import asyncio
 from kasa import SmartPlug
 
-plug = SmartPlug("10.0.0.180")
-asyncio.run(plug.update())
-plug.alias
+
+async def main():
+    p = SmartPlug("10.0.0.180")
+
+    await p.update()  # Request the update
+    print(p.alias)  # Print out the alias
+    print(p.emeter_realtime)  # Print out current emeter status
+
+    await p.turn_off()  # Turn the device off
+
+if __name__ == "__main__":
+    asyncio.run(main())
