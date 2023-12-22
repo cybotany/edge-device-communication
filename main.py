d28f99eec246518fd2b4ceb91bcacf242396290d
Raphael <puello322@gmail.com>
2023-12-22 00:03:41 -0700

    more fixes

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import RPi.GPIO as GPIO
 from pn532 import *
 
+def extract_uid(pages):
+    """ Extract the 7-byte UID from the first 9 bytes of memory. """
+    # UID is the first 7 bytes of the combined pages
+    return pages[:7]
+
 if __name__ == '__main__':
     try:
         pn532 = PN532_SPI(debug=False, reset=20, cs=4)
@@ -13,25 +18,31 @@ if __name__ == '__main__':
 
         print('Waiting for RFID/NFC card...')
         uid_list = []
-        last_uid = None  # Variable to store the last read UID
+        last_uid = None
 
         while True:
             uid = pn532.read_passive_target(timeout=0.5)
             if uid is None:
                 continue
-            if uid != last_uid:  # Check if the read UID is different from the last UID
-                last_uid = uid  # Update the last UID
-                if uid not in uid_list:
-                    uid_list.append(uid)
-                    print('Found new card. UID:', [hex(i) for i in uid])
-                    for i in range(30):
-                        try:
-                            print(i, ':', ' '.join(['%02X' % x
-                                for x in pn532.ntag2xx_read_block(i)]))
-                        except Exception as e:
-                            print(e)
-                else:
-                    print('Found duplicate card. UID:', [hex(i) for i in uid])
+            if uid != last_uid:
+                last_uid = uid
+                try:
+                    # Read the first 3 blocks (9 bytes)
+                    page0 = pn532.ntag2xx_read_block(0)
+                    page1 = pn532.ntag2xx_read_block(1)
+                    page2 = pn532.ntag2xx_read_block(2)
+                    combined_pages = page0 + page1 + page2[:1]  # First 9 bytes
+
+                    extracted_uid = extract_uid(combined_pages)
+                    uid_hex = [hex(i) for i in extracted_uid]
+
+                    if uid_hex not in uid_list:
+                        uid_list.append(uid_hex)
+                        print('Found new card. Extracted UID:', uid_hex)
+                    else:
+                        print('Found duplicate card. Extracted UID:', uid_hex)
+                except Exception as e:
+                    print("Error reading tag:", e)
     except Exception as e:
         print(e)
     finally:
