32f0db0c11500964baaacf6514d512c621f582ec
Raphael <puello322@gmail.com>
2023-12-21 23:23:54 -0700

    more updates

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import RPi.GPIO as GPIO
 from pn532 import *
 
-def unique_uid(uid, uid_list):
-    """ Check if UID is unique in the list """
-    return not any(uid == existing_uid for existing_uid in uid_list)
-
 if __name__ == '__main__':
     try:
         pn532 = PN532_SPI(debug=False, reset=20, cs=4)
@@ -17,18 +13,19 @@ if __name__ == '__main__':
 
         print('Waiting for RFID/NFC card...')
         uid_list = []
+        last_uid = None  # Variable to store the last read UID
+
         while True:
-            # Check if a card is available to read
             uid = pn532.read_passive_target(timeout=0.5)
-            print('.', end="")
-            # Try again if no card is available.
             if uid is None:
                 continue
-            if unique_uid(uid, uid_list):
-                uid_list.append(uid)
-                print('Found new card with UID:', [hex(i) for i in uid])
-            else:
-                print('Duplicate card detected. UID:', [hex(i) for i in uid])
+            if uid != last_uid:  # Check if the read UID is different from the last UID
+                last_uid = uid  # Update the last UID
+                if uid not in uid_list:
+                    uid_list.append(uid)
+                    print('Found new card with UID:', [hex(i) for i in uid])
+                else:
+                    print('Duplicate card detected. UID:', [hex(i) for i in uid])
     except Exception as e:
         print(e)
     finally:
