c828a6f89d4f433cd48ec2c063c751cae0494e55
Raphael <puello322@gmail.com>
2023-12-25 15:05:21 -0700

    more updates

--- pn532/pn532.py
+++ pn532/pn532.py
@@ -31,133 +31,29 @@ The main difference is the interfaces implements.
 import RPi.GPIO as GPIO
 import time
 
-# Constants
-_PREAMBLE                      = 0x00
-_STARTCODE1                    = 0x00
-_STARTCODE2                    = 0xFF
-_POSTAMBLE                     = 0x00
-_HOSTTOPN532                   = 0xD4
-_PN532TOHOST                   = 0xD5
-_WAKEUP                        = 0x55
-_ISO14443A                     = 0x00
-_VALIDATIONBIT                 = 0x80
-_ACK                           = b'\x00\x00\xFF\x00\xFF\x00'
-_FRAME_START                   = b'\x00\x00\xFF'
+from .utils.contants import (PN532_ERRORS,
+                             _PREAMBLE,
+                             _STARTCODE,
+                             _POSTAMBLE,
+                             _HOSTTOPN532,
+                             _ACK,
+                             _NACK,
+                             _ISO14443A,
+                             _PN532_CMD_INDATAEXCHANGE,
+                             _PN532_CMD_GETFIRMWAREVERSION,
+                             _PN532_CMD_SAMCONFIGURATION,
+                             _PN532_CMD_INLISTPASSIVETARGET,
+                             _NTAG_CMD_GET_VERSION,
+                             _NTAG_CMD_READ,
+                             _NTAG_CMD_FAST_READ,
+                             _NTAG_CMD_COMPATIBILITY_WRITE,
+                             _NTAG_CMD_WRITE,
+                             _NTAG_CMD_READ_CNT,
+                             _NTAG_ADDR_READ_CNT,
+                             _NTAG_CMD_PWD_AUTH,
+                             _NTAG_CMD_READ_SIG,
+                             _NTAG_ADDR_READ_SIG)
 
-# PN532 Commands
-_COMMAND_DIAGNOSE              = 0x00
-_COMMAND_GETFIRMWAREVERSION    = 0x02
-_COMMAND_GETGENERALSTATUS      = 0x04
-_COMMAND_READREGISTER          = 0x06
-_COMMAND_WRITEREGISTER         = 0x08
-_COMMAND_READGPIO              = 0x0C
-_COMMAND_WRITEGPIO             = 0x0E
-_COMMAND_SETSERIALBAUDRATE     = 0x10
-_COMMAND_SETPARAMETERS         = 0x12
-_COMMAND_SAMCONFIGURATION      = 0x14
-_COMMAND_POWERDOWN             = 0x16
-_COMMAND_RFCONFIGURATION       = 0x32
-_COMMAND_RFREGULATIONTEST      = 0x58
-_COMMAND_INJUMPFORDEP          = 0x56
-_COMMAND_INJUMPFORPSL          = 0x46
-_COMMAND_INLISTPASSIVETARGET   = 0x4A
-_COMMAND_INATR                 = 0x50
-_COMMAND_INPSL                 = 0x4E
-_COMMAND_INDATAEXCHANGE        = 0x40
-_COMMAND_INCOMMUNICATETHRU     = 0x42
-_COMMAND_INDESELECT            = 0x44
-_COMMAND_INRELEASE             = 0x52
-_COMMAND_INSELECT              = 0x54
-_COMMAND_INAUTOPOLL            = 0x60
-_COMMAND_TGINITASTARGET        = 0x8C
-_COMMAND_TGSETGENERALBYTES     = 0x92
-_COMMAND_TGGETDATA             = 0x86
-_COMMAND_TGSETDATA             = 0x8E
-_COMMAND_TGSETMETADATA         = 0x94
-_COMMAND_TGGETINITIATORCOMMAND = 0x88
-_COMMAND_TGRESPONSETOINITIATOR = 0x90
-_COMMAND_TGGETTARGETSTATUS     = 0x8A
-
-# NTAG Commands
-NTAG_CMD_GET_VERSION                = 0x60
-NTAG_CMD_READ                       = 0x30
-NTAG_CMD_FAST_READ                  = 0x3A
-NTAG_CMD_WRITE                      = 0xA2
-NTAG_CMD_COMPATIBILITY_WRITE        = 0xA0
-NTAG_CMD_READ_CNT                   = 0x39
-NTAG_ADDR_READ_CNT                  = 0x02
-NTAG_CMD_PWD_AUTH                   = 0x1B
-NTAG_CMD_READ_SIG                   = 0x3C
-NTAG_ADDR_READ_SIG                  = 0x00
-
-# NDEF Record Types
-NDEF_URIPREFIX_NONE                 = 0x00
-NDEF_URIPREFIX_HTTP_WWWDOT          = 0x01
-NDEF_URIPREFIX_HTTPS_WWWDOT         = 0x02
-NDEF_URIPREFIX_HTTP                 = 0x03
-NDEF_URIPREFIX_HTTPS                = 0x04
-NDEF_URIPREFIX_TEL                  = 0x05
-NDEF_URIPREFIX_MAILTO               = 0x06
-NDEF_URIPREFIX_FTP_ANONAT           = 0x07
-NDEF_URIPREFIX_FTP_FTPDOT           = 0x08
-NDEF_URIPREFIX_FTPS                 = 0x09
-NDEF_URIPREFIX_SFTP                 = 0x0A
-NDEF_URIPREFIX_SMB                  = 0x0B
-NDEF_URIPREFIX_NFS                  = 0x0C
-NDEF_URIPREFIX_FTP                  = 0x0D
-NDEF_URIPREFIX_DAV                  = 0x0E
-NDEF_URIPREFIX_NEWS                 = 0x0F
-NDEF_URIPREFIX_TELNET               = 0x10
-NDEF_URIPREFIX_IMAP                 = 0x11
-NDEF_URIPREFIX_RTSP                 = 0x12
-NDEF_URIPREFIX_URN                  = 0x13
-NDEF_URIPREFIX_POP                  = 0x14
-NDEF_URIPREFIX_SIP                  = 0x15
-NDEF_URIPREFIX_SIPS                 = 0x16
-NDEF_URIPREFIX_TFTP                 = 0x17
-NDEF_URIPREFIX_BTSPP                = 0x18
-NDEF_URIPREFIX_BTL2CAP              = 0x19
-NDEF_URIPREFIX_BTGOEP               = 0x1A
-NDEF_URIPREFIX_TCPOBEX              = 0x1B
-NDEF_URIPREFIX_IRDAOBEX             = 0x1C
-NDEF_URIPREFIX_FILE                 = 0x1D
-NDEF_URIPREFIX_URN_EPC_ID           = 0x1E
-NDEF_URIPREFIX_URN_EPC_TAG          = 0x1F
-NDEF_URIPREFIX_URN_EPC_PAT          = 0x20
-NDEF_URIPREFIX_URN_EPC_RAW          = 0x21
-NDEF_URIPREFIX_URN_EPC              = 0x22
-NDEF_URIPREFIX_URN_NFC              = 0x23
-
-PN532_ERRORS = {
-    0x01: 'PN532 ERROR TIMEOUT',
-    0x02: 'PN532 ERROR CRC',
-    0x03: 'PN532 ERROR PARITY',
-    0x04: 'PN532 ERROR COLLISION_BITCOUNT',
-    0x05: 'PN532 ERROR MIFARE_FRAMING',
-    0x06: 'PN532 ERROR COLLISION_BITCOLLISION',
-    0x07: 'PN532 ERROR NOBUFS',
-    0x09: 'PN532 ERROR RFNOBUFS',
-    0x0a: 'PN532 ERROR ACTIVE_TOOSLOW',
-    0x0b: 'PN532 ERROR RFPROTO',
-    0x0d: 'PN532 ERROR TOOHOT',
-    0x0e: 'PN532 ERROR INTERNAL_NOBUFS',
-    0x10: 'PN532 ERROR INVAL',
-    0x12: 'PN532 ERROR DEP_INVALID_COMMAND',
-    0x13: 'PN532 ERROR DEP_BADDATA',
-    0x14: 'PN532 ERROR MIFARE_AUTH',
-    0x18: 'PN532 ERROR NOSECURE',
-    0x19: 'PN532 ERROR I2CBUSY',
-    0x23: 'PN532 ERROR UIDCHECKSUM',
-    0x25: 'PN532 ERROR DEPSTATE',
-    0x26: 'PN532 ERROR HCIINVAL',
-    0x27: 'PN532 ERROR CONTEXT',
-    0x29: 'PN532 ERROR RELEASED',
-    0x2a: 'PN532 ERROR CARDSWAPPED',
-    0x2b: 'PN532 ERROR NOCARD',
-    0x2c: 'PN532 ERROR MISMATCH',
-    0x2d: 'PN532 ERROR OVERCURRENT',
-    0x2e: 'PN532 ERROR NONAD',
-}
 
 class PN532Error(Exception):
     """
@@ -194,11 +90,34 @@ class PN532:
         """
         raise NotImplementedError
 
+    def _call_function(self, command, response_length=0, params=None, timeout=1):
+        """
+        Send specified command to the PN532
+        """
+        if params is None:
+            params = []
+        data = bytearray(2 + len(params))
+        data[0] = _HOSTTOPN532
+        data[1] = command & 0xFF
+        for i, val in enumerate(params):
+            data[2+i] = val
+        try:
+            self._write_frame(data)
+        except OSError:
+            self._wakeup()
+            return None
+        if not self._wait_for_ack(timeout):
+            return None
+        if not self._wait_ready(timeout):
+            return None
+        response = self._read_frame(response_length + 2)
+        return response[2:]
+
     def _get_firmware_version(self):
         """
         Retrieve the firmware version from the PN532.
         """
-        response = self._call_function(_COMMAND_GETFIRMWAREVERSION,
+        response = self._call_function(_PN532_CMD_GETFIRMWAREVERSION,
                                        response_length=4,
                                        timeout=0.5)
         if response is None:
@@ -259,46 +178,63 @@ class PN532:
         if not data or not 1 < len(data) <= length:
             raise ValueError(f'Data must be an array of 1 to {length} bytes.')
 
-    def _handle_frame(self, data, operation='build'):
+    def _build_frame(self, packet_data=None, frame_type='normal'):
         """
         Handle the construction or parsing of a frame.
 
         :param data: The data for building or parsing.
-        :param operation: 'build' for building a frame, 'parse' for parsing.
+        :param frame: The type of frame used to perform actions between the host and PN532.
         :return: Constructed or parsed frame.
         """
-        if operation == 'build':
-            length = len(data)
-            frame = bytearray(length + 7)
-            frame[0] = _PREAMBLE
-            frame[1] = _STARTCODE1
-            frame[2] = _STARTCODE2
-            checksum = sum(frame[0:3])
-            frame[3] = length & 0xFF
-            frame[4] = (~length + 1) & 0xFF
-            frame[5:-2] = data
-            checksum += sum(data)
-            frame[-2] = ~checksum & 0xFF
-            frame[-1] = _POSTAMBLE
-            return frame
-        elif operation == 'parse':
-            offset = 0
-            while data[offset] == 0x00:
-                offset += 1
-                if offset >= len(data):
-                    raise RuntimeError('Response frame preamble does not contain 0x00FF!')
-            if data[offset] != 0xFF:
+        frame = _PREAMBLE + _STARTCODE
+        if frame_type == 'normal' and packet_data is not None:
+            packet_length = len(packet_data)
+            self._validate_data_length(packet_data, 255)
+            packet_length_byte = packet_length.to_bytes(1, byteorder='big')
+            frame += packet_length_byte
+
+            packet_length_checksum = (~packet_length + 1 & 0xFF).to_bytes(1, byteorder='big')
+            frame += packet_length_checksum
+            
+            frame += packet_data
+
+            checksum = (sum(packet_data) + sum(_PREAMBLE + _STARTCODE + packet_length_byte + packet_length_checksum)) & 0xFF
+            checksum_byte = (~checksum & 0xFF).to_bytes(1, byteorder='big')
+            frame += checksum_byte
+        elif frame_type == 'ack':
+            frame += _ACK
+        elif frame_type == 'nack':
+            frame += _NACK
+        elif frame_type == 'error':
+            pass
+        frame += _POSTAMBLE
+        return frame
+   
+    def _parse_frame(self, packet_data):
+        """
+        Handle the construction or parsing of a frame.
+
+        :param data: The data for building or parsing.
+        :param frame: The type of frame used to perform actions between the host and PN532.
+        :return: Constructed or parsed frame.
+        """
+        offset = 0
+        while packet_data[offset] == 0x00:
+            offset += 1
+            if offset >= len(packet_data):
+                raise RuntimeError('Response frame preamble does not contain 0x00FF!')
+            if packet_data[offset] != 0xFF:
                 raise RuntimeError('Response frame preamble does not contain 0x00FF!')
             offset += 1
-            if offset >= len(data):
+            if offset >= len(packet_data):
                 raise RuntimeError('Response contains no data!')
-            frame_len = data[offset]
-            if (frame_len + data[offset+1]) & 0xFF != 0:
+            frame_len = packet_data[offset]
+            if (frame_len + packet_data[offset+1]) & 0xFF != 0:
                 raise RuntimeError('Response length checksum did not match length!')
-            checksum = sum(data[offset+2:offset+2+frame_len+1]) & 0xFF
+            checksum = sum(packet_data[offset+2:offset+2+frame_len+1]) & 0xFF
             if checksum != 0:
                 raise RuntimeError('Response checksum did not match expected value.')
-            parsed_data =  data[offset+2:offset+2+frame_len]
+            parsed_data =  packet_data[offset+2:offset+2+frame_len]
             return parsed_data
 
     def _write_frame(self, data):
@@ -306,7 +242,7 @@ class PN532:
         Write a frame to the PN532.
         """
         self._validate_data_length(data, 255)
-        frame = self._handle_frame(data, operation='build')
+        frame = self._build_frame(data, 'normal')
         self._write_data(bytes(frame))
 
     def _read_frame(self, length):
@@ -314,7 +250,7 @@ class PN532:
         Read a response frame from the PN532.
         """
         response = self._read_data(length + 7)
-        return self._handle_frame(response, operation='parse')
+        return self._parse_frame(response)
 
     def _wait_for_ack(self, timeout):
         """
@@ -323,49 +259,33 @@ class PN532:
         :param timeout: Time to wait for the ACK response.
         :return: True if ACK received, False otherwise.
         """
+        ack_frame = self._build_frame(frame_type='ack')
         start_time = time.time()
         while (time.time() - start_time) < timeout:
-            ack = self._read_data(len(_ACK))
-            if ack == _ACK:
+            ack = self._read_data(len(ack_frame))
+            if ack == ack_frame:
                 return True
         return False
 
-    def _call_function(self, command, response_length=0, params=None, timeout=1):
-        """
-        Send specified command to the PN532
-        """
-        if params is None:
-            params = []
-        data = bytearray(2 + len(params))
-        data[0] = _HOSTTOPN532
-        data[1] = command & 0xFF
-        for i, val in enumerate(params):
-            data[2+i] = val
-        data
-        try:
-            self._write_frame(data)
-        except OSError:
-            self._wakeup()
-            return None
-        if not self._wait_for_ack(timeout):
-            return None
-        if not self._wait_ready(timeout):
-            return None
-        response = self._read_frame(response_length + 2)
-        return response[2:]
-
     def SAM_configuration(self):
         """
-        Configure the PN532 for NTAG2xx reading.
-        """
-        self._call_function(_COMMAND_SAMCONFIGURATION,
-                            params=[0x01, 0x14, 0x01])
+        Configure the PN532 to select the data flow path by configuring
+        the internal serial data switch.
 
-    def read_passive_target(self, card_baud=_ISO14443A, timeout=1):
+        :param mode: The method of using the SAM (Security Access Module).
+        :param timeout: Defines the time-out only in Virtual card configuration.
+        :param irq: Specifies if the PN532 takes care of the P70_IRQ pin or not.
+        """
+        self._call_function(_PN532_CMD_SAMCONFIGURATION, params=[0x01, 0x14, 0x01])
+
+
+    def list_passive_target(self, card_baud=_ISO14443A, timeout=1):
         """
         Wait for an NTAG to be available and return its UID when found.
+        The goal of this command is to detect as many targets (maximum MaxTg)
+        as possible in passive mode. 
         """
-        response = self._call_function(_COMMAND_INLISTPASSIVETARGET,
+        response = self._call_function(_PN532_CMD_INLISTPASSIVETARGET,
                                        params=[0x01, card_baud],
                                        response_length=19,
                                        timeout=timeout)
@@ -373,96 +293,13 @@ class PN532:
             return None
         return response[6:6 + response[5]]
 
-    def ntag2xx_get_version(self):
-        """
-        Send the GET_VERSION command to an NTAG chip and return its version information.
-
-        :return: Tuple with version information or None if no response.
-        """
-        response = self._call_function(_COMMAND_INDATAEXCHANGE,
-                                       params=[0x01, NTAG_CMD_GET_VERSION & 0xFF],
-                                       response_length=8,
-                                       timeout=1)
-        if response[0]:
-            raise PN532Error(response[0])
-            return None
-        return response[1:]
-
-    def ntag2xx_read(self, start_page):
-        """
-        Read 16 bytes from the specified start page.
-        """
-        response = self._call_function(_COMMAND_INDATAEXCHANGE,
-                                      params=[0x01, NTAG_CMD_READ, start_page & 0xFF],
-                                      response_length=20)
-        if response[0]:
-            raise PN532Error(response[0])
-        return response[1:]
-
-    def ntag2xx_fast_read(self, start_page, end_page):
-        """
-        Read consecutive pages from start_page to end_page.
-        """
-        response = self._call_function(_COMMAND_INDATAEXCHANGE,
-                                      params=[0x01, NTAG_CMD_FAST_READ, start_page & 0xFF, end_page & 0xFF],
-                                      response_length=(end_page - start_page + 1) * 4 + 2)
-        if response[0]:
-            raise PN532Error(response[0])
-        return response[1:]
-
-    def ntag2xx_compatibility_write(self, page, data):
-        """
-        Write 4 bytes to a specific page using Compatibility Write.
-        """
-        self._validate_data_length(data, 4)
-        response = self._call_function(_COMMAND_INDATAEXCHANGE,
-                                      params=[0x01, NTAG_CMD_COMPATIBILITY_WRITE, page & 0xFF] + list(data),
-                                      response_length=1)
-        if response[0]:
-            raise PN532Error(response[0])
-        return response[0] == 0x00
-
-    def ntag2xx_read_cnt(self):
-        """
-        Read the NTAG NFC counter value.
-        """
-        response = self._call_function(_COMMAND_INDATAEXCHANGE,
-                                      params=[0x01, NTAG_CMD_READ_CNT, NTAG_ADDR_READ_CNT],
-                                      response_length=4)
-        if response[0]:
-            raise PN532Error(response[0])
-        return response[1:]
-
-    def ntag2xx_pwd_auth(self, password):
-        """
-        Authenticate with the NTAG using a password.
-        """
-        self._validate_data_length(password, 4)
-        response = self._call_function(_COMMAND_INDATAEXCHANGE,
-                                      params=[0x01, NTAG_CMD_PWD_AUTH] + list(password),
-                                      response_length=2)
-        if response[0]:
-            raise PN532Error(response[0])
-        return response[1] == 0x00
-
-    def ntag2xx_read_sig(self):
-        """
-        Read the NTAG signature.
-        """
-        response = self._call_function(_COMMAND_INDATAEXCHANGE,
-                                      params=[0x01, NTAG_CMD_READ_SIG, NTAG_ADDR_READ_SIG],
-                                      response_length=34)
-        if response[0]:
-            raise PN532Error(response[0])
-        return response[1:]
-
     def ntag2xx_write_block(self, block_number, data):
         """
         Write a block of data to the card.
         """
         self._validate_data_length(data, 4)
-        response = self._call_function(_COMMAND_INDATAEXCHANGE,
-                                      params=[0x01, NTAG_CMD_WRITE, block_number & 0xFF, data],
+        response = self._call_function(_PN532_CMD_INDATAEXCHANGE,
+                                      params=[0x01, _NTAG_CMD_WRITE, block_number & 0xFF, data],
                                       response_length=1)
         if response[0]:
             raise PN532Error(response[0])
@@ -472,8 +309,8 @@ class PN532:
         """
         Read a block of data from the card.
         """
-        response = self._call_function(_COMMAND_INDATAEXCHANGE,
-                                      params=[0x01, NTAG_CMD_READ, block_number & 0xFF],
+        response = self._call_function(_PN532_CMD_INDATAEXCHANGE,
+                                      params=[0x01, _NTAG_CMD_READ, block_number & 0xFF],
                                       response_length=17)
         if response[0]:
             raise PN532Error(response[0])
