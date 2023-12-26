4f946c00e3b80f4f176d70562035d67ddade696b
Raphael <puello322@gmail.com>
2023-12-26 15:54:22 -0700

    trying to integrate with app

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import requests
 import RPi.GPIO as GPIO
 import pn532.pn532 as nfc
 from pn532 import PN532_SPI
+
+API_URL = 'http://10.0.0.218:8080/api/create/link/'
 
 if __name__ == '__main__':
     try:
@@ -15,18 +18,25 @@ if __name__ == '__main__':
             if uid is None:
                 continue
             if uid != last_uid:
-                last_uid = uid
-                uid_hex = [hex(i) for i in uid]
-                if uid_hex not in uid_list:
-                    uid_list.append(uid_hex)
-                    print('Found new card. Extracted UID:', uid_hex)                        
+                # Convert UID to string format
+                uid_str = ':'.join(['{:02X}'.format(i) for i in uid])
+                if uid_str not in uid_list:
+                    uid_list.append(uid_str)
+                    print('Found new card. Extracted UID:', uid_str)
+                    # Send UID to Django API
+                    try:
+                        response = requests.post(API_URL, data={'uid': uid_str})
+                        if response.status_code == 201:
+                            print('Link created successfully in Django app.')
+                        else:
+                            print('Failed to create link in Django app:', response.text)
+                    except requests.exceptions.RequestException as e:
+                        print('Error communicating with Django app:', e)                 
                 else:
-                    print('Found duplicate card. Extracted UID:', uid_hex)
+                    print('Found duplicate card. Extracted UID:', uid_str)
                 
                 try:
                     record1 = pn532.create_ndef_record(tnf=0x01, record_type='T', payload='Testing', record_position='only')
-                    #record2 = pn532.create_ndef_record(tnf=0x01, record_type='U', payload='microsoft.com', record_position='middle')
-                    #record3 = pn532.create_ndef_record(tnf=0x01, record_type='U', payload='yahoo.com', record_position='last')
                     ndef_message = pn532.combine_ndef_records([record1])                   
                     pn532.write_ndef_message(ndef_message)
                     ntag_data = pn532.ntag2xx_dump(start_block=4, end_block=9)
