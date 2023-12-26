3ff40ef7e107ca2e7adefca078bbf99925fed431
Raphael <puello322@gmail.com>
2023-12-25 20:54:07 -0700

    still trying

--- pn532/pn532.py
+++ pn532/pn532.py
@@ -94,6 +94,8 @@ class PN532:
         packet_data[1] = command & 0xFF
         for i, val in enumerate(params):
             packet_data[2+i] = val
+        if self.debug:
+            print('Packet data: ', [hex(i) for i in packet_data])
         try:
             self._write_frame(packet_data)
         except OSError:
