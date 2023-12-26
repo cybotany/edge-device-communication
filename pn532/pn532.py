74115df7be040ff960107c198aa9b679699cbb5e
Raphael <puello322@gmail.com>
2023-12-25 21:10:28 -0700

    more fixes yet

--- pn532/pn532.py
+++ pn532/pn532.py
@@ -226,8 +226,8 @@ class PN532:
         Write a frame to the PN532.
         """
         frame = self._build_frame(packet_data)
-        if self.debug:
-            print('Write frame: ', [hex(i) for i in frame])
+        #if self.debug:
+        #    print('Write frame: ', [hex(i) for i in frame])
         self._write_data(bytes(frame))
 
     def _read_frame(self, length):
@@ -235,8 +235,8 @@ class PN532:
         Read a response frame from the PN532.
         """
         response = self._read_data(length + 7)
-        if self.debug:
-            print('Read frame:', [hex(i) for i in response])
+        #if self.debug:
+        #    print('Read frame:', [hex(i) for i in response])
         parsed_data = self._parse_frame(response)
         return parsed_data
 
@@ -246,8 +246,8 @@ class PN532:
         """
         ack = self._read_data(len(_ACK))
         if ack == _ACK:
-            if self.debug:
-                print('Received ACK.')
+            #if self.debug:
+            #    print('Received ACK.')
             return True
         return False
 
@@ -322,6 +322,6 @@ class PN532:
                                       response_length=bytes_returned)
         if response[0]:
             raise PN532Error(response[0])
-        if self.debug:
-            print('Read block:', [hex(i) for i in response[1:]])
+        #if self.debug:
+        #    print('Read block:', [hex(i) for i in response[1:]])
         return response[1:]
