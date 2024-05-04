5fc4674b13fa354f0cec0c0adac849cc37220e69
Raphael <rafael.e.puello@gmail.com>
2024-05-03 19:47:18 -0600

    Refactor main.py to use authenticate_user and register_ntag functions

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import RPi.GPIO as GPIO
 from pn532 import PN532_SPI as PN532
 from ntag import NTAG
 
-def _authenticate_user():
+def authenticate_user():
     username = os.getenv('USERNAME')
     password = os.getenv('PASSWORD')
     auth_url = os.getenv('AUTH_URL')
@@ -20,7 +20,7 @@ def _authenticate_user():
     except requests.exceptions.RequestException as e:
         sys.exit(f"Error during authentication request, exiting program. Error was: {e}")
 
-def _register_ntag(token, uid):
+def register_ntag(token, uid):
     api_url = os.getenv('API_URL')
     headers = {'Authorization': f'Bearer {token}'}
     payload = {
@@ -43,9 +43,9 @@ def main():
     try:
         pn532 = PN532(debug=True, reset=20, cs=4)
         pn532.SAM_configuration()
-        ntag = NTAG(pn532, debug=True)
+        #ntag = NTAG(pn532, debug=True)
 
-        token = _authenticate_user()
+        token = authenticate_user()
         uid_list = []
         last_uid = None
         print('Waiting for an NFC card...')
@@ -58,7 +58,7 @@ def main():
                     uid_list.append(uid)
                     print(f'Found new card. Extracted UID: {uid}')
 
-                    ntag_url = _register_ntag(token, uid)
+                    ntag_url = register_ntag(token, uid)
                     if ntag_url:
                         print(ntag_url)
                     #    record = ntag.create_ndef_record(tnf=0x01, record_type='U', payload=ntag_url)
