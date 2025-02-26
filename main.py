ddb709679fd00b5db2c208e4876d8ee7a95de640
Raphael <rafael.e.puello@gmail.com>
2025-02-26 15:25:43 -0700

    Refactor authentication to use email instead of username and simplify NTAG registration payload

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ from pn532 import PN532_SPI as PN532
 from ntag import NTAG
 
 def authenticate_user():
-    username = os.getenv('USERNAME')
+    email = os.getenv('EMAIL')
     password = os.getenv('PASSWORD')
     auth_url = os.getenv('AUTH_URL')
     try:
-        response = requests.post(auth_url, data={'username': username, 'password': password})
+        response = requests.post(auth_url, json={'email': email, 'password': password})
         if response.status_code == 200:
             return response.json().get('access')
         else:
@@ -21,29 +21,21 @@ def authenticate_user():
         sys.exit(f"Error during authentication request, exiting program. Error was: {e}")
 
 def register_ntag(token, uid):
-    """
-      "ID"         "Tag Type"
-        1	    "Plant Label (Indoor)"
-        2	    "Plant Label (Outdoor)"
-        5	    "Pet Tag (27mm)"
-        7	    "Pet Tag (30mm)"
-        8	    "Dry Inlay (38mm)"
-    """
     api_url = os.getenv('API_URL')
     headers = {'Authorization': f'Bearer {token}'}
-    payload = {
-        'serial_number': uid,
-        'tag_type_id': 1
-    }
+    payload = {'uid': uid}
     
     try:
         response = requests.post(api_url, headers=headers, json=payload, verify=True)
         if response.status_code == 201 or response.status_code == 200:
-            uuid = response.json().get('uuid')
-            return uuid
+            return response.json().get('uuid')
+        elif response.status_code == 409:
+            print("NTAG already registered")
+            return None
         else:
             print(f"Failed to register or update NTAG. Status code: {response.status_code}, Error: {response.text}")
             return None
+
     except requests.exceptions.RequestException as e:
         print(f"Error communicating with NTAG API: {e}")
         return None
@@ -57,12 +49,13 @@ def main():
         token = authenticate_user()
         uid_list = []
         last_uid = None
+
         print('Waiting for an NFC card...')
         while True:
-            serial_number = pn532.list_passive_target(timeout=0.5)
-            if serial_number and serial_number != last_uid:
-                last_uid = serial_number
-                uid = ''.join(['{:02X}'.format(i) for i in serial_number])
+            uid = pn532.list_passive_target(timeout=0.5)
+            if uid and uid != last_uid:
+                last_uid = uid
+                uid = ''.join(['{:02X}'.format(i) for i in uid])
                 if uid not in uid_list:
                     uid_list.append(uid)
                     print(f'Found new card. Extracted UID: {uid}')
@@ -71,7 +64,6 @@ def main():
                     if ntag_uuid:
                         ntag_uuid = uuid.UUID(ntag_uuid)
                         ntag_pwd = ntag_uuid.hex[:8]
-                        ntag.set_password(ntag_pwd)
                         success = ntag.write_ndef()
                         if success:
                             print(f'Wrote NDEF message to NTAG. Password: {ntag_pwd}')
