6982d3622d439d7d5345896bd1fd0203cc4288b8
Raphael <rafael.e.puello@gmail.com>
2025-02-25 20:48:02 -0700

    Refactor NTAG class initialization and configuration methods for clarity and improved structure

--- ntag/ntag.py
+++ ntag/ntag.py
@@ -1,46 +1,57 @@
 from .constants import (_NTAG_CMD_READ, _NTAG_CMD_WRITE)
 
-
 class NTAG:
     def __init__(self, pn532, debug=False):
-        # Initialize memory: 45 pages, 4 bytes per page
+        """
+        Initialize the NTAG instance.
+
+        Args:
+            pn532: An instance responsible for NFC communication.
+            debug (bool): Flag to enable debug output.
+        """
         self.pn532 = pn532
         self.debug = debug
-        self.memory = [[0x00 for _ in range(4)] for _ in range(45)]
+        # Initialize memory: 45 pages with 4 bytes per page
+        self.memory = [[0x00] * 4 for _ in range(45)]
         self.password = None
         self.record_type = 'U'
         self.tnf = 0x01
-        self.url = 'digidex.tech/links/'
-        self.set_initial_configurations()
+        # Configure mirror and modulation settings
+        self.mirror_conf = 0b11  # Enable both UID and NFC counter ASCII mirror
+        self.mirror_byte = 0b00  # Start mirroring at the 1st byte of the page
+        self.strong_mod_en = 0b1 # Enable strong modulation mode
+        self.mirror_page = 0x0C  # What page the mirror starts
+        self.auth0 = 0x05        # Password protection enabled from this page
+        # Configure protection and NFC counter settings
+        self.prot = 0b0              # Write access not protected (password verification disabled)
+        self.cfglock = 0b0           # Configuration remains open to write access
+        self.nfc_cnt_en = 0b1        # Enable NFC counter
+        self.nfc_cnt_pwd_prot = 0b0  # Disable password protection for NFC counter
+        self.authlim = 0b000         # No limitation on negative password attempts
+        self.rfu = 0x00              # RFU (Reserved for Future Use)
+        self.url = 'digidex.tech/links/?m='
+        self._set_initial_configurations()
 
-    def set_initial_configurations(self):
+    def _set_initial_configurations(self):
         """
-        Set pre-programmed capabilities and NDEF magic number for NTAG213.
+        Configure pre-programmed capabilities and NDEF magic numbers for NTAG213.
         """
+        # Set NDEF magic numbers and default configuration pages
         self.memory[3] = [0xE1, 0x10, 0x12, 0x00]
         self.memory[4] = [0x01, 0x03, 0xA0, 0x0C]
         self.memory[5] = [0x34, 0x03, 0x00, 0xFE]
 
-        mirror_conf = 0b11 # MIRROR_CONF: Set to 11b to enable both UID and NFC counter ASCII mirror
-        mirror_byte = 0b00 # MIRROR_BYTE: Set to 00b to start mirroring at the 1st byte of the page
-        strong_mod_en = 0b1 # STRG_MOD_EN: Set to 1b to enable strong modulation mode
+        # Configure mirror and modulation settings (Page 41)
         self.memory[41] = [
-            (mirror_conf << 6) | (mirror_byte << 4) | (strong_mod_en << 2),  # MIRROR_CONF, MIRROR_BYTE, STRG_MOD_EN
-            0x00,  # RFU (Reserved for Future Use)
-            0x0C,  # MIRROR_PAGE (Page 11)
-            0x05   # AUTH0 (Password protection enabled from page 5)
+            (self.mirror_conf << 6) | (self.mirror_byte << 4) | (self.strong_mod_en << 2),
+            self.rfu, self.mirror_page, self.auth0
         ]
-        
-        prot = 0b0 # PROT: Set to 0b to protect write access with password verification
-        cfglck = 0b0 # CFGLCK: Set to 0b to keep configuration open to write access
-        nfc_cnt_en = 0b1 # NFC_CNT_EN: Set to 1b to enable NFC counter
-        nfc_cnt_pwd_prot = 0b0 # NFC_CNT_PWD_PROT: Set to 0b to disable password protection for NFC counter
-        authlim = 0b000 # AUTHLIM: Set to 000b to disable limitation of negative password attempts
+
+        # Configure protection and NFC counter settings (Page 42)
         self.memory[42] = [
-            (prot << 7) | (cfglck << 6) | (nfc_cnt_en << 4) | (nfc_cnt_pwd_prot << 3) | authlim,
-            0x00, 0x00, 0x00   # RFU (Reserved for Future Use)
+            (self.prot << 7) | (self.cfglock << 6) | (self.nfc_cnt_en << 4) | (self.nfc_cnt_pwd_prot << 3) | self.authlim,
+            self.rfu, self.rfu, self.rfu
         ]
-        return True
         
     def write_block(self, block_number, data):
         """
@@ -133,12 +144,13 @@ class NTAG:
         tlv = b'\x34' + tlv_type + tlv_length + complete_record + b'\xFE'  # Append terminator
         return tlv
 
-    def create_ndef_record(self, payload=None):
+    def create_ndef_record(self, payload=None, id=''):
         """
         Method to create the NDEF record with debug statements.
         """
         if not payload:
             payload = self.url
+            
         message_flags = self._create_message_flags(payload, self.tnf)
         prepared_payload = self._prepare_payload(self.record_type, payload)
         if self.debug:
